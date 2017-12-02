cbb390dca4ef629f5b0754a027b9d078e6141271
AssassinLard <34082414+AssassinLard@users.noreply.github.com>
2017-12-02 12:19:20 +0800

    Update BNode.java

--- BNode.java
+++ BNode.java
@@ -4,7 +4,6 @@ public class BNode {
     int count; //Determines # of Keys in Node
     int key[]; //Array of Key Values
     BNode child[]; //Array of references;
-    String val[]; //Array of String values
     boolean leaf; //Boolean to check whether Node is leaf or not
     BNode parent; //Parent of current Node
     
@@ -17,15 +16,7 @@ public class BNode {
         child = new BNode[order]; //size of References array
         leaf = true; //Assumes every Node is a leaf at first
         count = 0; //Remains 0 until Keys are added        
-    }    
-    
-    public void updateValue(String n, int dex){
-        val[dex] = n;   
-    }
-    
-    public String getValue(int dex){
-        return val[dex];   
-    }
+    }   
     
     public int getKey(int dex){ //Returns Key value at specified index
         return key[dex];
