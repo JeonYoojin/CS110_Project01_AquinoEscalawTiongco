a627f2752570ba6868962ab5a06b1a8c9fceebc6
AssassinLard <34082414+AssassinLard@users.noreply.github.com>
2017-12-07 00:51:42 +0800

    Update btdb.java

--- btdb.java
+++ btdb.java
@@ -2,13 +2,10 @@ import java.util.*;
 import java.io.*;
 
 public class btdb{
-
 	public static void main(String[] args) throws IOException{
 		Scanner read = new Scanner(System.in);
-
 		BTree tree = new BTree(args[0]);
-		ValuesEdit values = new ValuesEdit(args[1]);
-
+		Values values = new Values(args[1]);
 		while(true){
 			System.out.print("> "); //the pointer thing for inputs
 			String[] input = read.nextLine().split("\\s"); // splits input by spaces???
@@ -17,12 +14,9 @@ public class btdb{
 				long amt = values.amtRecord();
 				if(tree.searchNodes(key,tree.findRoot()) == -1){ //means it doesn't exist yet
 					String word = "";
-				
 					for(int i = 2; i < input.length; i++){
 						word = word + input[i] + " ";
-					}
-					
-					
+					}					
 					tree.insert(key,tree.findRoot(),amt);
 					values.insertEntry(word);
 					System.out.printf("< %d inserted.\n",key);
@@ -31,23 +25,23 @@ public class btdb{
 					System.out.printf("ERROR: %d already exists.\n",key);
 				}
 				
-			} else if(input[0].equals("update")){
+			} 
+			else if(input[0].equals("update")){
 				long key = Long.parseLong(input[1]);
 				String word = "";
 				long offset = tree.searchNodes(key,tree.findRoot());
-				
 				for(int i = 2; i < input.length; i++){
-						word = word + input[i] + " ";
-				}
-				
+					word = word + input[i] + " ";
+				}				
 				if(offset != -1){
-					values.updateEntry(key,word);
+					values.updateEntry(word,offset);
 					System.out.printf("< %d updated.\n", key);
 				}
 				else{
 					System.out.println("ERROR: key does not exist.");
 				}
-			} else if(input[0].equals("select")){
+			} 
+			else if(input[0].equals("select")){
 				long key = Long.parseLong(input[1]);
 				String word = "";
 				long offset = tree.searchNodes(key,tree.findRoot());
@@ -57,13 +51,13 @@ public class btdb{
 				else{
 					System.out.println("ERROR: key does not exist.");
 				}
-			} else if(input[0].equals("exit")){
+			} 
+			else if(input[0].equals("exit")){
 				break;
-			} else{
-				System.out.println("ERROR: invalid command.");
+			} 
+			else{
+			System.out.println("ERROR: invalid command.");
 			}
 		}
-
 	}
-
 }
